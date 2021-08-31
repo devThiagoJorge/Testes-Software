a76eb1001ecff267a28a905148d7968f0cfabdf7
Thiago Jorge <thiago.v.jorge@hotmail.com>
2021-08-31 11:18:21 -0300

    first commit

---
+++ Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorDaSoma()
+        { 
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(3, 3);
+
+            //Assert
+            Assert.Equal(6, resultado);
+        }
+
+        [Fact]
+        public void Calculadora_Dividir_RetornarValorDaDivisao()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Dividir(4, 2);
+
+            //Assert
+            Assert.Equal(2, resultado);
+        }
+
+        [Theory]
+        [InlineData(1,1,2)]
+        [InlineData(3,3,6)]
+        [InlineData(10,10,20)]
+        public void Calculadora_Somar_RetornarValoresSomaCorreta(double v1, double v2, double total)
+        {
+            //Arrange
+            var calculadora = new Calculadora();
+
+            //Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            // Assert
+            Assert.Equal(total, resultado);
+        }
+    }
+}
---
+++ Demo/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public double Dividir(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
